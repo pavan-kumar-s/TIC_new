816c35e592b419bbe8166c4015def1a55c29366f
pavan-kumar-s <spavaniitm@smail.iitm.ac.in>
2025-03-03 18:47:58 +0530

    updated

---
+++ Feasbility_based/time_comparison_with_prev_enum.m
@@ -0,0 +1,8 @@
+clear
+load('D:\OneDrive_moved_files\Microbial_community_gap_filling\With_AGORA2\AGORA2\Brevibacillus_brevis_FJAT_0809_GLX.mat')
+tic
+[TICs1,Direction1] = ThermOptEnumMILP_feas(model);
+toc
+tic
+[TICs,Direction] = ThermOptEnumMILP_rev(model);
+toc
